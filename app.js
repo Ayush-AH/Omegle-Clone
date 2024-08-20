28e9629a2e47e5eaa7dc96790ab02ae699715f2b
Ayush Ahirwar <ayushahirwar6673@gmail.com>
2024-08-20 21:32:53 +0530

    all setup done

--- app.js
+++ app.js
@@ -1,16 +1,61 @@
 const express = require("express")
 const app = express()
 const path = require("path")
+const server = require("http").createServer(app)
+const io = require("socket.io")(server)
+const { v4: uuidv4 } = require("uuid")
 
-app.set("view engine","ejs")
-app.use(express.static(path.join(__dirname,"public")))
+
+app.set("view engine", "ejs")
+app.use(express.static(path.join(__dirname, "public")))
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
+
+const waitingUser = []
 
 
-app.get("/",function(req,res){
+io.on("connection", function (socket) {
+    socket.on("join-room", function () {
+        if (waitingUser.length > 0) {
+            var roomId = uuidv4()
+            socket.join(roomId)
+            waitingUser[0].join(roomId)
+            waitingUser.pop()
+            io.to(roomId).emit("joined",roomId)
+        }
+        else {
+            waitingUser.push(socket)
+        }
+    })
+
+    socket.on("message",function(data){
+        socket.broadcast.to(data.room).emit("reseved-message", data)
+    })
+
+    socket.on("start-call",function(room){
+        socket.broadcast.to(room).emit("incoming-call")
+    })
+    socket.on("accepted",function(room){
+        socket.broadcast.to(room).emit("accepted")
+    })
+    socket.on("signalingMessage",function(data){
+        socket.broadcast.to(data.room).emit("signalingMessage", data)
+    })
+
+    socket.on("user-disconnect",function(room){
+        if(waitingUser.indexOf(socket) !== -1){
+            waitingUser.pop()
+        }
+        socket.broadcast.to(room).emit("user-disconnect")
+    })
+})
+
+app.get("/", function (req, res) {
     res.render("index")
 })
 
+app.get("/chat", function (req, res) {
+    res.render("chat")
+})
 
-app.listen(3000)
+server.listen(3000)
---
+++ public/javascripts/script.js
@@ -0,0 +1,191 @@
+const socket = io()
+var room;
+var messageContainer = document.querySelector("#message-container")
+//webRTC 
+var localStream;
+var remoteStream;
+var peerConnection;
+var stunServer = {
+    iceServers: [{ urls: "stun:stun.l.google.com:19302" }]
+}
+
+
+
+
+
+
+socket.emit("join-room")
+
+socket.on("joined", function (roomId) {
+    room = roomId
+    document.querySelector("#connecting").classList.add("hidden")
+    document.querySelector("#message-container").classList.remove("hidden")
+})
+
+//messages
+document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault()
+    var message = document.querySelector("#message-input").value
+    messageContainer.innerHTML += `
+      <div class="flex items-center justify-end py-1">
+                    <div class="p-2 rounded-tr-none rounded bg-blue-600 text-white">
+                        <p>${message}</p>
+                    </div>
+                </div>`
+    document.querySelector("#message-input").value = ""
+
+    socket.emit("message", { room, message })
+
+})
+
+socket.on("reseved-message", function (data) {
+    messageContainer.innerHTML += `
+     <div class="flex items-center justify-start py-1">
+                    <div class="p-2 rounded-tl-none rounded bg-zinc-100 text-black">
+                        <p>${data.message}</p>
+                    </div>
+                </div>`
+})
+
+
+
+//video call start
+document.querySelector("#start-call").addEventListener("click", function () {
+    socket.emit("start-call", room)
+})
+
+socket.on("incoming-call", function () {
+    document.querySelector("#call-panel").classList.remove("hidden")
+})
+
+socket.on("signalingMessage", async function (data) {
+    var { type, offer, answer, candidate } = data
+    if (type === "candidate") {
+        if (peerConnection) {
+            await peerConnection.addIceCandidate(candidate)
+        } else {
+            console.warn("Received candidate, but peerConnection is not initialized.");
+        }
+    }
+    if (type === "offer") {
+        if (!peerConnection) {
+            await createPeerConnection();
+        }
+        await peerConnection.setRemoteDescription(offer)
+        var answer = await peerConnection.createAnswer()
+        await peerConnection.setLocalDescription(answer)
+        socket.emit("signalingMessage", { type: "answer", room, answer })
+    }
+    if (type === "answer") {
+       if(!peerConnection.currentRemoteDescription){
+        await peerConnection.setRemoteDescription(answer)
+       }
+    }
+    if(type === "endcall"){
+        handleEndCall()
+    }
+})
+
+async function initilize() {
+
+    localStream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: true
+    })
+
+    await createPeerConnection()
+    var offer = await peerConnection.createOffer()
+    peerConnection.setLocalDescription(offer)
+    socket.emit("signalingMessage", { type: "offer", room, offer })
+}
+
+async function createPeerConnection() {
+    peerConnection = new RTCPeerConnection(stunServer)
+    peerConnection.onicecandidate = event => {
+        if (event.candidate) {
+            socket.emit("signalingMessage", { type: "candidate", room, candidate: event.candidate })
+        }
+    }
+
+    localStream.getTracks().forEach(track => peerConnection.addTrack(track, localStream))
+    document.querySelector("#localVideo").srcObject = localStream
+
+    remoteStream = new MediaStream()
+    document.querySelector("#remoteVideo").srcObject = remoteStream
+    peerConnection.ontrack = event => event.streams[0].getTracks().forEach(track => remoteStream.addTrack(track))
+}
+//accept call
+document.querySelector("#accept-call").addEventListener("click", function () {
+    document.querySelector("#call-panel").classList.add("hidden")
+    document.querySelector("#video-panel").classList.remove("hidden")
+    socket.emit("accepted", room)
+    initilize()
+})
+
+socket.on("accepted", function () {
+    initilize()
+    document.querySelector("#video-panel").classList.remove("hidden")
+})
+
+//reject call
+document.querySelector("#reject-call").addEventListener("click", function () {
+    document.querySelector("#call-panel").classList.add("hidden")
+})
+
+//end call
+document.querySelector("#callbtn").addEventListener("click",function(){
+    handleEndCall()
+    socket.emit("signalingMessage",{type:"endcall",room})
+})
+//mute
+document.querySelector("#mutebtn").addEventListener("click",function(){
+    var mutedbtn = document.querySelector("#mutebtn")
+    handleControlles(0, "ri-mic-fill", "ri-mic-off-fill", mutedbtn)
+})
+//camera
+document.querySelector("#camerabtn").addEventListener("click",function(){
+    var camerabtn = document.querySelector("#camerabtn")
+    handleControlles(1, "ri-video-on-fill", "ri-video-off-fill", camerabtn)
+})
+
+function handleControlles(index, onIcon, offIcon, controllbtn) {
+    localStream.getTracks()[index].enabled = !localStream.getTracks()[index].enabled
+    if (!localStream.getTracks()[index].enabled) {
+        controllbtn.classList.add("not-active")
+        controllbtn.querySelector("i").classList.remove(onIcon)
+        controllbtn.querySelector("i").classList.add(offIcon)
+    }
+    else if (localStream.getTracks()[index].enabled) {
+        controllbtn.classList.remove("not-active")
+        controllbtn.querySelector("i").classList.add(onIcon)
+        controllbtn.querySelector("i").classList.remove(offIcon)
+    }
+
+}
+
+//disconnect
+window.addEventListener("beforeunload", function () {
+    socket.emit("user-disconnect", room)
+})
+
+socket.on("user-disconnect", function () {
+    room = ""
+    messageContainer.classList.add("hidden")
+    document.querySelector("#connecting").classList.remove("hidden")
+    handleEndCall()
+    socket.emit("join-room")
+})
+
+function handleEndCall() {
+    if (peerConnection) {
+        peerConnection.close();
+        peerConnection = null;
+    }
+    if (localStream) {
+        localStream.getTracks().forEach(track => track.stop());
+    }
+    if (remoteStream) {
+        remoteStream = null;
+    }
+    document.querySelector("#video-panel").classList.add("hidden");
+}
