60c538bb76bbe7170270edb8abd5215cc406dcff
Ayush Ahirwar <ayushahirwar6673@gmail.com>
2024-08-24 13:11:08 +0530

    optimized ths code

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -10,10 +10,6 @@ var stunServer = {
 }
 
 
-
-
-
-
 socket.emit("join-room")
 
 socket.on("joined", function (roomId) {
@@ -48,7 +44,6 @@ socket.on("reseved-message", function (data) {
 })
 
 
-
 //video call start
 document.querySelector("#start-call").addEventListener("click", function () {
     socket.emit("start-call", room)
@@ -60,49 +55,52 @@ socket.on("incoming-call", function () {
 
 socket.on("signalingMessage", async function (data) {
     var { type, offer, answer, candidate } = data
-    if (type === "candidate") {
-        if (peerConnection) {
-            await peerConnection.addIceCandidate(candidate)
-        } else {
-            console.warn("Received candidate, but peerConnection is not initialized.");
-        }
+
+    if (type === "candidate" && peerConnection) {
+        await peerConnection.addIceCandidate(candidate)
     }
+
     if (type === "offer") {
-        if (!peerConnection) {
-            await createPeerConnection();
-        }
         await peerConnection.setRemoteDescription(offer)
         var answer = await peerConnection.createAnswer()
         await peerConnection.setLocalDescription(answer)
         socket.emit("signalingMessage", { type: "answer", room, answer })
     }
     if (type === "answer") {
-       if(!peerConnection.currentRemoteDescription){
-        await peerConnection.setRemoteDescription(answer)
-       }
+        if (!peerConnection.currentRemoteDescription) {
+            await peerConnection.setRemoteDescription(answer)
+        }
     }
-    if(type === "endcall"){
+    if (type === "endcall") {
         handleEndCall()
     }
 })
 
-async function initilize() {
-
-    localStream = await navigator.mediaDevices.getUserMedia({
-        audio: true,
-        video: true
-    })
+async function initialize(isCaller) {
+    try {
+        localStream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: true
+        })
+    }
+    catch (err) {
+        console.error("Error accessing media devices:", err)
+    }
 
     await createPeerConnection()
-    var offer = await peerConnection.createOffer()
-    peerConnection.setLocalDescription(offer)
-    socket.emit("signalingMessage", { type: "offer", room, offer })
+    if (isCaller) {
+        var offer = await peerConnection.createOffer()
+        peerConnection.setLocalDescription(offer)
+        socket.emit("signalingMessage", { type: "offer", room, offer })
+    }
 }
 
 async function createPeerConnection() {
     peerConnection = new RTCPeerConnection(stunServer)
     peerConnection.onicecandidate = event => {
         if (event.candidate) {
+            console.log("sending candidate");
+
             socket.emit("signalingMessage", { type: "candidate", room, candidate: event.candidate })
         }
     }
@@ -119,11 +117,11 @@ document.querySelector("#accept-call").addEventListener("click", function () {
     document.querySelector("#call-panel").classList.add("hidden")
     document.querySelector("#video-panel").classList.remove("hidden")
     socket.emit("accepted", room)
-    initilize()
+    initialize(false)
 })
 
 socket.on("accepted", function () {
-    initilize()
+    initialize(true)
     document.querySelector("#video-panel").classList.remove("hidden")
 })
 
@@ -133,17 +131,17 @@ document.querySelector("#reject-call").addEventListener("click", function () {
 })
 
 //end call
-document.querySelector("#callbtn").addEventListener("click",function(){
+document.querySelector("#callbtn").addEventListener("click", function () {
     handleEndCall()
-    socket.emit("signalingMessage",{type:"endcall",room})
+    socket.emit("signalingMessage", { type: "endcall", room })
 })
 //mute
-document.querySelector("#mutebtn").addEventListener("click",function(){
+document.querySelector("#mutebtn").addEventListener("click", function () {
     var mutedbtn = document.querySelector("#mutebtn")
     handleControlles(0, "ri-mic-fill", "ri-mic-off-fill", mutedbtn)
 })
 //camera
-document.querySelector("#camerabtn").addEventListener("click",function(){
+document.querySelector("#camerabtn").addEventListener("click", function () {
     var camerabtn = document.querySelector("#camerabtn")
     handleControlles(1, "ri-video-on-fill", "ri-video-off-fill", camerabtn)
 })
